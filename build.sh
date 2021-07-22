9dac683d7d9cea2fefe1bc139d24d6826f5730b0
Matthew Carter <mattcat@amazon.com>
2021-07-22 11:00:16 -0700

    Add Go Module Support
    Go 1.16 now enables Go Modules by default. The addition of the go.mod
    and go.sum files now provides support for Go Modules.
    
    Signed-off-by: Matthew Carter <mattcat@amazon.com>
    Signed-off-by: Jon Oikawa <joikawa@amazon.com>
    Signed-off-by: Jun'ichi Okuda <okudajun@amazon.com>

--- build.sh
+++ build.sh
@@ -10,12 +10,6 @@ VERSION=$(git describe --always --tags)
 echo -e "Commit date: ${COMMITDATE}"
 echo -e "Version: ${VERSION}"
 
-# Go get dependencies
-echo "Getting dependencies..."
-go get -u github.com/BurntSushi/toml
-go get -u github.com/google/go-cmp/cmp
-go get -u github.com/digineo/go-ping
-
 # Build for darwin/amd64
 echo "Running go build..."
 GOOS=darwin GOARCH=amd64 CGO_ENABLED=0 go build -trimpath -ldflags="-s -w -X 'main.CommitDate=${COMMITDATE}' -X 'main.Version=${VERSION}'"
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/aws/ec2-macos-init
+
+go 1.16
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/digineo/go-ping v1.0.1
+	github.com/google/go-cmp v0.5.6
+	github.com/stretchr/testify v1.7.0
+)

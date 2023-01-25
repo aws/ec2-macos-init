ff45c0c4b2a8b4f72d5bef18bd9d6e699d2f1121
Jacob Vallejo <jakeev@amazon.com>
2023-01-25 10:54:47 -0800

    refactor: quiet down main

---
+++ version.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+var (
+	// CommitDate is the date of the commit used at build time.
+	CommitDate string
+	// Version is the ec2-macos-init release version for this build.
+	Version string = "0.0.0-dev"
+)
+
+// printVersion prints the output for the version command.
+func printVersion() {
+	const gitHubLink  = "https://github.com/aws/ec2-macos-init"
+
+	fmt.Printf("\nEC2 macOS Init\n"+
+		"Version: %s [%s]\n"+
+		"%s\n"+
+		"Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.\n\n",
+		Version, CommitDate, gitHubLink,
+	)
+}
